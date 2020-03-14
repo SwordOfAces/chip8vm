d4ad856f67e496a4f4467c6218de40157a3970c0
RJ <r.j.kobish@gmail.com>
2020-03-14 18:49:23 -0400

    Refactor ROM loading.
    Pulls loading the ROM into memory into its own function, greatly
    cleaning main. Additionally, data gets written to the VM memory in a
    much more straight-forward and terse way with fread.

--- chip8vm.c
+++ chip8vm.c
@@ -17,51 +17,28 @@ typedef struct {
 }
 chip8_state;
 
+void load_rom(char *romfilename, chip8_state *state);
 
 int main(int argc, char *argv[]){
+    // Ensure that we're being used with what we'll assume is a romfile
     if (argc != 2)
     {
         printf("Usage: chip8vm <romfile>\n");
         exit(1);
     }
 
-    chip8_state state;
+    //  Create a pointer to a state, so we can modify it in functions
+    chip8_state *state = malloc(sizeof(chip8_state));
     // Initialize important fields
     // It's not important for say, registers to be cleared
     // and graphics will undoubtably be immediately cleared with 0x00e0 op 
-    memset(state.memory, 0, sizeof(state.memory));
-    state.pc = 0x200;
-    state.sp = 0x0;
-    memset(state.key, 0, sizeof(state.key));
+    memset(state->memory, 0, sizeof(state->memory));
+    state->pc = 0x200;
+    state->sp = 0x0;
+    memset(state->key, 0, sizeof(state->key));
 
-    // Open a romfile
-    FILE *romfile = fopen(argv[1], "r");
-    if (romfile == NULL)
-    {
-        printf("Could not open file: %s\n", argv[1]);
-        exit(1);
-    }
-
-    // Fill our memory with program data, starting at 0x200
-    for (int i = 0x200; i < 0x1000; i++)
-    {
-        char c = fgetc(romfile);
-        if (c != EOF)
-        {
-            state.memory[i] = c;
-        }
-        // If we've reached the EOF, this for loop takes over to fill the
-        // remainder with 0s, and then breaks from the for loop
-        else
-        {
-            for (int j = i; j < 0xFFF; j++)
-            {
-                state.memory[j] = 0x000;
-            }
-            break;
-        }
-    }
-    fclose(romfile);
+    // Load given romfile
+    load_rom(argv[1], state);
 
     // Dump our memory contents to the console (TEMP)
     for (int i = 0; i < 256; i++)
@@ -69,9 +46,30 @@ int main(int argc, char *argv[]){
         printf("%03x: ", i * 16);
         for (int j = 0; j < 16; j++)
         {
-            printf("%02x ", state.memory[i * 16 + j]);
+            printf("%02x ", state->memory[i * 16 + j]);
         }
         printf("\n");
     }
     printf("Memory dumped\n");
+
+
+    // Destroy the state
+    free(state);
+}
+
+
+// Load a rom into vm memory
+void load_rom(char *romfilename, chip8_state *state)
+{
+    // Open a romfile
+    FILE *romfile = fopen(romfilename, "r");
+    if (romfile == NULL)
+    {
+        printf("Could not open file: %s\n", romfilename);
+        exit(1);
+    }
+
+    // Fill our memory with program data, starting at 0x200
+    // 0x1000 total memory - 0x200 reserved = 0xe00 for rom 
+    fread(state->memory + 0x200, 1, 0xe00, romfile);
 }
