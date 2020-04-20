cb6fae36271284fce3705eb9f980146e168e126b
RJ <r.j.kobish@gmail.com>
2020-04-19 20:30:14 -0400

    Remove old copies of testingsys.* due to renaming

--- test.h
+++
@@ -1,9 +0,0 @@
-#ifndef TESTINGSYS_H_INC
-#define TESTINGSYS_H_INC
-
-//include "chip8vm.h"
-
-int test_op(chip8_state *state, unsigned short t_val, unsigned short e_val, char dump);
-int test_suite(chip8_state *state);
-
-#endif
