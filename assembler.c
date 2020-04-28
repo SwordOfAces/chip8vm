8fae5ecd830c8ed15bf187ccabb4b0b581820930
RJ <r.j.kobish@gmail.com>
2020-04-28 15:44:23 -0400

    Assembler partial creation.
    Creation of assembler, with support for opcodes up through 0x7XNN.
    These have been tested but test hasn't yet been made to automatically
    check against correct results. This method of doing things is non-ideal,
    of course, as it's dependent on counting character indices and any
    change to my assembly languge will break this. Regex in C sounds horrid
    given the reputation regexes have, and C is already bad with strings.
    Rewriting the assembly mnemonics for fixed command width is "more"
    appealing, but not what I am interested in, as this assembly is mainly
    for testing.

---
+++ assembler.c
@@ -0,0 +1,139 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> // for memset
+
+void invalid_line(char *line, int line_no);
+int conv_hex_str(char *str, int len);
+void write_op(unsigned short opcode, FILE *outfile);
+
+int main(int argc, char *argv[]){
+    if (argc < 2)
+    {
+        printf("Usage: assembler <codefile>\n");
+        // TODO option to name out file
+        exit(1);
+    }
+
+    // Open an assembly code file
+    FILE *codefile = fopen(argv[1], "r");
+    if (codefile == NULL)
+    {
+        printf("Could not open file: %s\n", argv[1]);
+        exit(1);
+    }
+
+    // Open a file for our bytecode:
+    // If creating name change option, be sure to change print text
+    FILE *outfile = fopen("out.ch8", "w");
+    if (outfile == NULL)
+    {
+        printf("Could not create out.ch8\n");
+    }
+    
+    // Line limit 50 is fine, even if we want commenting
+    char line[50]; 
+    // compare first n chars. has to be at least 5 with current mnemonics
+    int line_no = 0; // useful for error reporting
+
+    // some declarations for placeholders (val == nn, addr == nnn)
+    int x, y, val, addr;
+    unsigned short opcode;
+
+    while (fgets(line, sizeof(line), codefile) != NULL)
+    {
+        line_no++;
+        // Comments start with %, let's head those off right away
+        if (line[0] == '%')
+            continue;
+        // Can't use a switch with strings, so if-else if-else ladder
+        // Compare strings by their first few characters
+        // Enough to guarantee correct, not enough to get to args
+        else if (strncmp(line, "OR", 2) == 0)
+        {
+            x = conv_hex_str(&line[4], 1);
+            y = conv_hex_str(&line[7], 1);
+            opcode = 0x8001 + (x * 0x100) + (y * 0x10);
+        }
+        // From here, can examine first 5 chars, handled by opcode order:
+        else if (strncmp(line, "CALLPROG", 5) == 0)
+        {
+            addr = conv_hex_str(&line[10], 3);
+            opcode = addr;
+        }
+        else if (strncmp(line, "CLRS", 4) == 0)
+            opcode = 0x00e0;
+        else if (strncmp(line, "RET", 3) == 0)
+        {
+            opcode = 0x00ee;
+        }
+        else if (strncmp(line, "GOTO", 4) == 0)
+        {
+            addr = conv_hex_str(&line[6], 3);
+            opcode = 0x1000 + addr;
+        }
+        else if (strncmp(line, "CALL ", 5) == 0)
+        {
+            addr = conv_hex_str(&line[6], 3);
+            opcode = 0x2000 + addr;
+        }
+        else if (strncmp(line, "TEQ.I", 5) == 0)
+        {
+            x = conv_hex_str(&line[7], 1);
+            val = conv_hex_str(&line[10], 2);
+            opcode = 0x3000 + (x * 0x100) + val;
+        }
+        else if (strncmp(line, "TNE.I", 5) == 0)
+        {
+            x = conv_hex_str(&line[7], 1);
+            val = conv_hex_str(&line[10], 2);
+            opcode = 0x4000 + (x * 0x100) + val;
+        }
+        else if (strncmp(line, "TEQ ", 4) == 0)
+        {
+            x = conv_hex_str(&line[5], 1);
+            y = conv_hex_str(&line[8], 1);
+            opcode = 0x5000 + (x * 0x100) + (y * 0x10);
+        }
+        else if (strncmp(line, "MOV.I", 5) == 0)
+        {
+            x = conv_hex_str(&line[7], 1);
+            val = conv_hex_str(&line[10], 2);
+            opcode = 0x6000 + (x * 0x100) + val;
+        }
+        else if (strncmp(line, "INC.I", 5) == 0)
+        {
+            x = conv_hex_str(&line[7], 1);
+            val = conv_hex_str(&line[10], 2);
+            opcode = 0x7000 + (x * 0x100) + val;
+        }
+
+
+        write_op(opcode, outfile);
+    }
+    fclose(codefile); 
+    fclose(outfile);
+}
+
+// Handles printing error messages when encountering an invalid mnemonic
+void invalid_line(char *line, int line_no)
+{
+    printf("ERROR at line: %i\n", line_no);
+    printf("ERROR: Invalid mnemonic: %s\n", line);
+    exit(1);
+}
+
+int conv_hex_str(char *str, int len)
+{
+    char sub[len];
+    strncpy(sub, str, len);
+    return strtol(str, NULL, 16);
+}
+
+void write_op(unsigned short opcode, FILE *outfile)
+{
+    printf("%04x\n", opcode);
+    unsigned char msb = (opcode & 0xff00) >> 8;
+    unsigned char lsb = opcode & 0xff;
+    fputc(msb, outfile);
+    fputc(lsb, outfile);
+}
